1efb33e0a16359c4d6c6d32c9bd2d0a3e5b9709f
fmdd00 <francisco63554@hotmail.com>
2022-10-24 17:58:54 -0500

    login formateado

--- client/src/components/body/auth/Login.js
+++ client/src/components/body/auth/Login.js
@@ -40,25 +40,33 @@ function Login() {
   return (
     <div className='container'>
       <Link to={'/'}><FontAwesomeIcon icon='fa-solid fa-arrow-left' title='Regresar' className='return' /></Link>
-      <div className='login_page'>
-        <h2>Iniciar Sesión</h2>
+      <div className='login_page shadow-lg p-3 rounded'>
+        <h2 className='fw-bold fs-1 text-center'>Iniciar Sesión</h2>
         {err && showErrMsg(err)}
         {success && showSuccessMsg(success)}
+        
         <form onSubmit={handleSubmit}>
-          <div>
-            <label htmlFor='email'>Correo</label>
-            <input type='text' placeholder='Ingresa tu correo' id='email' value={email} name='email' onChange={handleChangeInput}></input>
+          <div className='mb-3'>
+            <label className='fs-5 form-label' htmlFor='email'>Correo:</label>
+            <input type='email' className='form-control' placeholder='Ingresa tu correo' id='email' value={email} name='email' onChange={handleChangeInput}></input>
+            <div className='form-text'>* Nunca compartiremos tu dirección de email con otros.</div>
           </div>
-          <div>
-            <label htmlFor='password'>Contraseña</label>
-            <input type='password' placeholder='Ingresa tu contraseña' id='password' value={password} name='password' onChange={handleChangeInput}></input>
+          
+          <div className='mb-3'>
+            <label className='fs-5 form-label' htmlFor='password'>Contraseña:</label>
+            <input type='password' className='form-control' placeholder='Ingresa tu contraseña' id='password' value={password} name='password' onChange={handleChangeInput}></input>
+            <div className='form-text'>* Tu contraseña debe ser de 8-20 caracteres, contener letras y números, y no debe tener espacios, caracteres especiales o emojis.</div>
           </div>
-          <div className='row'>
-            <button type='submit'>Inicia Sesión</button>
+
+          <div className='mb-3'>
             <Link to='/forgot_password'>Olvidé mi contraseña</Link>
           </div>
+
+          <div className='d-grid gap-2 col-4 mx-auto mb-3'>
+            <button type='submit' className='btn btn-dark'>INICIA SESIÓN</button>
+          </div>
         </form>
-        <p>¿Aún no te has registrado? <Link to='/register'> Regístrate Aquí</Link></p>
+        <p className='text-center'>¿Aún no te has registrado? <Link to='/register' > Regístrate Aquí</Link></p>
       </div>
     </div>
   )
