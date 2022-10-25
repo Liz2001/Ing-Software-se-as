e737631559e8c403ce56bb4847b8a3591ab84c90
fmdd00 <francisco63554@hotmail.com>
2022-10-25 10:05:17 -0500

    se creo componente About para sprint 3

---
+++ client/src/components/body/about/About.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function About() {
+    return <div className="container">
+        <h1>La página 'Sobre Nosotros' debería ir aquí. jeje</h1>
+    </div>
+}
+
+export default About;
