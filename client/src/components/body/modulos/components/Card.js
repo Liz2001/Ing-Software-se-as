e2b0078de39dd9d7e83a839f31f93dfe31fbc3e1
fmdd00 <francisco63554@hotmail.com>
2022-10-25 18:22:14 -0500

    nada importante

--- client/src/components/body/modulos/components/Card.js
+++ client/src/components/body/modulos/components/Card.js
@@ -3,20 +3,19 @@ import Button from './Button'
 import { Link } from 'react-router-dom'
 
 
-
-function Card({titulo,cuerpo,disponible, completado,id}) {
-    const noMostrar = () =>{
-        console.log('no disponible')        
-    }
+function Card({ titulo, cuerpo, disponible, completado, id }) {
+  const noMostrar = () => {
+    console.log('no disponible')
+  }
   return (//Francisco aca se muestra el modulo
-    <div className="col-4">
+    <div className="col-4 mt-5">
       <div className="card">
         <div className="card-body">
           <h3 className="card-title fw-bold fs-3 text-center text-uppercase pb-2 border-bottom">Nivel {titulo}</h3>
-          <p style={{ textAlign:'justify' }} className="card-text">{cuerpo}</p>
-          {  disponible ? <Link to= {`/modulo/${id}`} className="btn btn-primary">Empezar módulo</Link>
-            : <Button  color= 'gray' text='Módulo bloqueado' onClick={noMostrar}/>
-          }          
+          <p style={{ textAlign: 'justify' }} className="card-text">{cuerpo}</p>
+          {disponible ? <Link to={`/modulo/${id}`} className="btn btn-primary">Empezar módulo</Link>
+            : <Button color='gray' text='Módulo bloqueado' onClick={noMostrar} />
+          }
         </div>
       </div>
     </div>
