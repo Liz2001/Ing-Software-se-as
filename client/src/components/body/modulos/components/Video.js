66d072f51762787644ea7b07b09c782852007938
sbanonf <sbanonf@gmail.com>
2022-10-25 23:11:23 -0500

    Comentado

--- client/src/components/body/modulos/components/Video.js
+++ client/src/components/body/modulos/components/Video.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Video = ({pregunta}) => {
-  return (////Francisco aca se muestra lo que es el Video.
+  return (
     <video controls width="100%">
     <source src={pregunta} type="video/mp4" /> 
     Sorry, your browser doesn't support embedded videos.
