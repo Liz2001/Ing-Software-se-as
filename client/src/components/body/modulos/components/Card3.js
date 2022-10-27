da5d72a59c7c87ee29c0cd0587d7fa35ff7925b3
fmdd00 <francisco63554@hotmail.com>
2022-10-27 10:27:19 -0500

    componente card actualizado

--- client/src/components/body/modulos/components/Card3.js
+++ client/src/components/body/modulos/components/Card3.js
@@ -1,9 +1,7 @@
 import React from "react";
 import Button from "./Button";
 import { Link } from "react-router-dom";
-import image1 from '../../../images/image1.jpg';
 import image2 from '../../../images/image2.jpg';
-import image3 from '../../../images/image3.jpg';
 
 function Card3({ titulo, cuerpo, disponible, completado, id }) {
   const noMostrar = () => {
@@ -12,23 +10,24 @@ function Card3({ titulo, cuerpo, disponible, completado, id }) {
 
   return (
     <div className="col-4 mt-3">
-      <div className="card h-100">
-        <img className="rounded img-fluid" src={ image2 } alt='imagen ref' />
+      <div className="card h-100 rounded-0">
+        <img className="img-fluid" src={ image2 } alt='imagen ref' />
         <div className="card-body">
-          <h4 className="card-title ">Nivel {titulo}</h4>
-          <p className="card-text text-secondary">{cuerpo}</p>
-          {disponible ? (
+          <h4 className="card-title fw-bold fs-3 text-center text-uppercase pb-2 border-bottom">Nivel {titulo}</h4>
+          <p style={{ textAlign: 'justify' }} className="card-text text-secondary">{cuerpo}</p>
+          
+        </div>
+      </div>
+      {disponible ? (
             <Link
               to={`/modulo/${id}`}
-              className="btn btn-primary d-grid col-6 mx-auto"
+              className="rounded-0 btn btn-primary d-grid mx-auto"
             >
               Iniciar
             </Link>
           ) : (
             <Button color="gray" text="Bloqueado" onClick={noMostrar} />
           )}
-        </div>
-      </div>
     </div>
   );
 }
